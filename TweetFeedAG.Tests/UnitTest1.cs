4ef2cbd109fb9e50c08aa129e9851f65639efc1c
solifassalimu <Solifas>
2022-12-07 20:53:42 +0200

    Creating an initial Project Template

---
+++ TweetFeedAG.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace TweetFeedAG.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ TweetFeedAG/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TweetFeedAG;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        //Console.ReadLine();
+    }
+}
+
